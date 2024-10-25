b66e8339e5a8ca13aeca5ba7652307226ab33985
betiiy-haile <betelhemhaile74@gmail.com>
2024-10-25 11:38:36 +0300

    testing credentais

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,7 +9,7 @@ export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientSecret: process.env.GOOGLE_SECRET,    
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
